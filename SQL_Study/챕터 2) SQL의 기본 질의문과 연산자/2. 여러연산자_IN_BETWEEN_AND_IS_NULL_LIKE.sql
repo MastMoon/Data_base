68f3a36b26e2883437d00dda73b445b2c25c1995
MastMoon <jihun0948@naver.com>
2025-03-20 21:10:03 +0900

    25.03.20

--- SQL_Study/챕터 2) SQL의 기본 질의문과 연산자/2. 여러연산자_IN_BETWEEN_AND_IS_NULL_LIKE.sql
+++ SQL_Study/챕터 2) SQL의 기본 질의문과 연산자/2. 여러연산자_IN_BETWEEN_AND_IS_NULL_LIKE.sql
@@ -31,13 +31,9 @@ SELECT *
 FROM 고객
 WHERE 담당자직위 <> '대표 이사';
 
-<<<<<<< HEAD:SQL_Study/챕터 2) SQL의 기본 질의문과 연산자/2. 여러연산자_IN_BETWEEN_AND_IS_NULL_LIKE.sql
-=======
 SELECT *
 FROM 고객
 WHERE 지역 = '';
-
-
 
 UPDATE 고객
 SET 지역 = NULL
@@ -48,12 +44,11 @@ FROM 고객
 WHERE 지역 IS NULL;
 
 -- [LIKE 연산자] 특정 패턴과 일치하는 문자열을 검색하는 연산자
--- 파일: LIKE 연산자.sql
+-- ==================================================
 SELECT *
 FROM 고객
 WHERE 도시 LIKE '%광역시'
 AND (고객번호 LIKE '_C%' OR 고객번호 LIKE '__C%');
->>>>>>> 7e2ac15292b36e4dbb7865a17dc21e04a500e5dd:SQL_Study/기본_문법/IN_BETWEEN_AND_IS_NULL_LIKE_여러연산자.sql
 
 -- [논리 연산자] AND, OR, NOT을 사용하여 논리적 조건을 조합하는 연산자
 -- ==================================================
@@ -61,35 +56,6 @@ SELECT *
 FROM 고객
 WHERE 도시 = '부산광역시'
 AND 마일리지 < 1000;
-
-<<<<<<< HEAD:SQL_Study/챕터 2) SQL의 기본 질의문과 연산자/2. 여러연산자_IN_BETWEEN_AND_IS_NULL_LIKE.sql
-=======
--- [비교 연산자] 두 값을 비교하여 TRUE 또는 FALSE를 반환하는 연산자
--- 파일: 비교_연산자.sql
-SELECT 23 >= 5
-	 , 23 <= 5
-     , 23 > 23
-	 , 23 < 23
-     , 23 = 23
-	 , 23 != 23
-     , 23 <> 23;
-
-
-
-SELECT *
-FROM 고객
-WHERE 담당자직위 <> '대표 이사';
-
--- [산술 연산자] 수학적 연산을 수행하는 연산자 (+, -, *, / 등)
--- 파일: 산술_연산자.sql
-SELECT 23 + 5 AS 더하기
-	 , 23 - 5 AS 빼기
-	 , 23 * 5 AS 곱하기
-	 , 23 / 5 AS 실수나누기
-	 , 23 DIV 5 AS 정수나누기
-	 , 23 % 5 AS 나머지1
-	 , 23 MOD 5 AS 나머지2;
->>>>>>> 7e2ac15292b36e4dbb7865a17dc21e04a500e5dd:SQL_Study/기본_문법/IN_BETWEEN_AND_IS_NULL_LIKE_여러연산자.sql
 
 -- [집합 연산자] 여러 SELECT 결과를 조합하는 UNION, INTERSECT, EXCEPT 연산자
 -- ==================================================
@@ -116,7 +82,6 @@ SELECT 고객번호
 FROM 고객
 WHERE 도시 = '부산광역시' OR 마일리지 < 1000
 ORDER BY 1;
-
 
 -- [IS NULL 연산자] NULL 값을 확인하는 연산자
 -- ==================================================
@@ -157,13 +122,6 @@ SELECT 담당자명
 FROM 고객
 WHERE 마일리지 >= 100000
 AND 마일리지 <= 200000;
+
 -- 같은 조건을 BETWEEN으로 표현
 -- WHERE 마일리지 BETWEEN 100000 AND 200000;
-
-
--- [LIKE 연산자] 특정 패턴과 일치하는 문자열을 검색하는 연산자
--- ==================================================
-SELECT *
-FROM 고객
-WHERE 도시 LIKE '%광역시'
-AND (고객번호 LIKE '_C%' OR 고객번호 LIKE '__C%');
