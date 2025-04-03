10bcaadd078384a82ff98990f4dfe7713d497c3a
Moon ji hun <jihun0948@naver.com>
2025-04-03 11:29:10 +0900

    25.04.03

--- SQL_Study/챕터 4) 함수2. 집계함수/3. 점검문제.sql
+++ SQL_Study/챕터 4) 함수2. 집계함수/3. 점검문제.sql
@@ -7,3 +7,36 @@ SELECT YEAR(주문일) AS 주문년도,
 	   COUNT(*) AS 주문건수
 FROM 주문
 GROUP BY 주문년도;
+
+
+SELECT YEAR(주문일) AS 주문년도,
+	   QUARTER(주문일) AS 분기,
+	   COUNT(*) AS 주문건수
+FROM 주문
+GROUP BY 주문년도, 분기
+WITH ROLLUP;
+
+
+SELECT MONTH(주문일) AS 주문월,
+	   COUNT(*) AS 주문건수
+
+FROM 주문
+WHERE 요청일 < 발송일
+GROUP BY 주문월
+ORDER BY 주문월;
+
+
+SELECT 제품명,
+	   SUM(재고) AS 재고합
+       
+FROM 제품
+WHERE 제품명 LIKE '%아이스크림%'
+GROUP BY 제품명;
+
+
+SELECT IF(마일리지 >= 50000, 'VIP고객', '일반고객') AS 고객구분,
+	   COUNT(*) AS 고객수,
+       AVG(마일리지) AS 평균마일리지
+       
+FROM 고객
+GROUP BY 고객구분;
